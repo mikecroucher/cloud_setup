ed85dd1d56bf9495627340624acb8f31c26f38ec
Mike Croucher <mikecroucher@users.noreply.github.com>
2019-05-21 08:33:51 +0100

    Update linux_build.sh

--- linux_build.sh
+++ linux_build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+# This is for Amazon Linux
 
 # Anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 bash ~/Anaconda3-2019.03-Linux-x86_64.sh -b -p $HOME/anaconda
 eval "$(~/anaconda/bin/conda shell.bash hook)"
 conda init
+
+#Google Chrome.  Intoli is a 3rd party site.  
+curl https://intoli.com/install-google-chrome.sh | bash

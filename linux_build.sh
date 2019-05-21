3ca611127416cd7535e010df36006de3fb16b2b2
Mike Croucher <mikecroucher@users.noreply.github.com>
2019-05-21 08:30:24 +0100

    Update linux_build.sh

--- linux_build.sh
+++ linux_build.sh
@@ -3,3 +3,5 @@
 # Anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
 bash ~/Anaconda3-2019.03-Linux-x86_64.sh -b -p $HOME/anaconda
+eval "$(~/anaconda/bin/conda shell.bash hook)"
+conda init
